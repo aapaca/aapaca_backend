ba5464fae972fc8fae93a9046fed390e03db2c4f
qiringji <qiringji@gmail.com>
2020-04-05 20:57:55 +0900

    update album class not to use Artist interface

--- domain/album.go
+++ domain/album.go
@@ -5,8 +5,8 @@ import "time"
 type Album struct {
 	ID int
 	Name string
-	PrimaryArtist Artist
-	AttendedArtist []Artist
+	PrimaryArtist interface{}
+	AttendedArtist []interface{}
 	Label string // レーベルも構造体にすべきかもしれない
 	ReleasedDate time.Time
 }
