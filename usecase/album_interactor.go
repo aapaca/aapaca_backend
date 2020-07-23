34216e9cf4c79eaa85ae06463e6c6697c086ffdc
marblet <guitarmarble0117@gmail.com>
2020-07-23 20:33:36 +0900

    implement usecase of album

---
+++ usecase/album_interactor.go
@@ -0,0 +1,12 @@
+package usecase
+
+import "../domain"
+
+type AlbumInteractor struct {
+	AlbumRepository AlbumRepository
+}
+
+func (interactor *AlbumInteractor) AlbumById(id int) (album domain.Album, err error) {
+	album, err = interactor.AlbumRepository.FindById(id)
+	return
+}
