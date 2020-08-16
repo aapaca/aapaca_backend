0e5128e956194a6e085626623569b9369fc095d9
marblet <guitarmarble0117@gmail.com>
2020-08-16 22:01:54 +0900

    replace AttendedArtists with Credits

--- src/app/domain/album.go
+++ src/app/domain/album.go
@@ -3,10 +3,10 @@ package domain
 import "time"
 
 type Album struct {
-	ID              int
-	Name            string
-	PrimaryArtist   interface{}
-	AttendedArtists []interface{}
-	Label           string // レーベルも構造体にすべきかもしれない
-	ReleasedDate    time.Time
+	ID            int
+	Name          string
+	PrimaryArtist interface{}
+	Credits       []Credit
+	Label         string // レーベルも構造体にすべきかもしれない
+	ReleasedDate  time.Time
 }
