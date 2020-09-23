f2461399d72878b09c81c399df88bdc7ffffcd19
marblet <guitarmarble0117@gmail.com>
2020-09-24 00:20:51 +0900

    subtest

--- src/app/usecase/album_interactor_test.go
+++ src/app/usecase/album_interactor_test.go
@@ -18,16 +18,18 @@ func (_m *AlbumRepositoryMock) FindById(id int) (domain.Album, error) {
 }
 
 func TestAlbumById(t *testing.T) {
-	rd, _ := time.Parse("2006-01-02", "2020-09-22")
-	testAlbum := domain.Album{ID: 1, Name: "Test T. Test", PrimaryArtist: domain.Artist{ID: 100}, Label: "Test Label", ReleasedDate: &rd, ImageURL: "https://www.example.com"}
-	albumRepositoryMock := new(AlbumRepositoryMock)
-	albumRepositoryMock.On("FindById", testAlbum.ID).Return(testAlbum, nil)
-	albumInteractor := &AlbumInteractor{
-		AlbumRepository: albumRepositoryMock,
-	}
-	got, err := albumInteractor.AlbumById(1)
-	if err != nil {
-		t.Error(err)
-	}
-	assert.Equal(t, testAlbum, got, "Error")
+	t.Run("1", func(t *testing.T) {
+		rd, _ := time.Parse("2006-01-02", "2020-09-22")
+		testAlbum := domain.Album{ID: 1, Name: "Test T. Test", PrimaryArtist: domain.Artist{ID: 100}, Label: "Test Label", ReleasedDate: &rd, ImageURL: "https://www.example.com"}
+		albumRepositoryMock := new(AlbumRepositoryMock)
+		albumRepositoryMock.On("FindById", testAlbum.ID).Return(testAlbum, nil)
+		albumInteractor := &AlbumInteractor{
+			AlbumRepository: albumRepositoryMock,
+		}
+		got, err := albumInteractor.AlbumById(1)
+		if err != nil {
+			t.Error(err)
+		}
+		assert.Equal(t, testAlbum, got, "Error")
+	})
 }
