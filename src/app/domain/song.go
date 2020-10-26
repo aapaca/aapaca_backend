eb4144a9426c492f1fbbc10f55245fd6d8617f25
marblet <guitarmarble0117@gmail.com>
2020-10-26 20:55:22 +0900

    add imageurl entity

--- src/app/domain/song.go
+++ src/app/domain/song.go
@@ -10,7 +10,8 @@ type Song struct {
 	Label         string // TODO レーベルも構造体にする
 	Albums        []Album
 	Genre         string // TODO ジャンルも構造体にする
-	ReleaseDate   *time.Time
+	ReleasedDate  *time.Time
 	Length        *time.Time // 曲の長さ
+	ImageURL      string
 	Links         map[string]string
 }
