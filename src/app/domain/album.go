9435dbd4608c0510b2cace479e4d108dd59ffd27
marblet <guitarmarble0117@gmail.com>
2020-11-17 15:16:35 +0900

    reformat

--- src/app/domain/album.go
+++ src/app/domain/album.go
@@ -3,13 +3,13 @@ package domain
 import "time"
 
 type Album struct {
-	ID            int         `json:"id"`
-	Name          string      `json:"name"`
-	PrimaryArtist interface{} `json:"primaryArtist,omitempty"`
-	Credits       []Credit    `json:"credits,omitempty"`
-	Label         string      `json:"label,omitempty"` // レーベルも構造体にすべきかもしれない
-	ReleasedDate  *time.Time  `json:"releasedDate,omitempty"`
-	ImageURL      string      `json:"imageUrl"`
-	Description   string      `json:"description,omitempty"`
+	ID            int               `json:"id"`
+	Name          string            `json:"name"`
+	PrimaryArtist interface{}       `json:"primaryArtist,omitempty"`
+	Credits       []Credit          `json:"credits,omitempty"`
+	Label         string            `json:"label,omitempty"` // レーベルも構造体にすべきかもしれない
+	ReleasedDate  *time.Time        `json:"releasedDate,omitempty"`
+	ImageURL      string            `json:"imageUrl"`
+	Description   string            `json:"description,omitempty"`
 	Links         map[string]string `json:"links,omitempty"`
 }
