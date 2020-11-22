4d2073622b90c44f8fe37885d8c19489c1574287
marblet <guitarmarble0117@gmail.com>
2020-11-22 13:00:40 +0900

    add order and songlen to song domain

--- src/app/domain/song.go
+++ src/app/domain/song.go
@@ -1,10 +1,14 @@
 package domain
 
+import "time"
+
 type Song struct {
 	ID            int               `json:"id"`
 	Name          string            `json:"name"`
 	PrimaryArtist interface{}       `json:"primaryArtist,omitempty"`
 	Credits       []Credit          `json:"credits,omitempty"`
+	SongLen       *time.Time        `json:"length,omitempty"`
+	Order         string            `json:"order,omitempty"`
 	Label         string            `json:"label,omitempty"` // TODO レーベルも構造体にする
 	Albums        []Album           `json:"albums,omitempty"`
 	Genre         string            `json:"genre,omitempty"` // TODO ジャンルも構造体にする
