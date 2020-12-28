8fedd5a9c1e916d26300d96aa71cc82e6f94f21b
marblet <guitarmarble0117@gmail.com>
2020-12-28 15:34:53 +0900

    separate external ids

--- mysql/initdb.d/1_main_tables.sql
+++ mysql/initdb.d/1_main_tables.sql
@@ -6,9 +6,6 @@ CREATE TABLE aapaca.artists (
     status INT NOT NULL, # 0 -> individual, 1 -> group
     image_url VARCHAR(256) DEFAULT 'http://placeimg.com/200/200/any' NOT NULL,
     description TEXT,
-    amazon_music_id VARCHAR(256) DEFAULT '' NOT NULL,
-    apple_music_id VARCHAR(256) DEFAULT '' NOT NULL,
-    spotify_id VARCHAR(256) DEFAULT '' NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -26,9 +23,6 @@ CREATE TABLE aapaca.albums (
     released_date DATE,
     image_url VARCHAR(256) DEFAULT 'http://placeimg.com/200/200/any' NOT NULL,
     description TEXT,
-    amazon_music_id VARCHAR(256) DEFAULT '' NOT NULL,
-    apple_music_id VARCHAR(256) DEFAULT '' NOT NULL,
-    spotify_id VARCHAR(256) DEFAULT '' NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (primary_artist_id) REFERENCES aapaca.artists(id)
 );
@@ -40,9 +34,21 @@ CREATE TABLE aapaca.songs (
     label VARCHAR(256) DEFAULT '' NOT NULL,
     genre VARCHAR(256) DEFAULT '' NOT NULL,
     song_len TIME,
-    amazon_music_id VARCHAR(256) DEFAULT '' NOT NULL,
-    apple_music_id VARCHAR(256) DEFAULT '' NOT NULL,
-    spotify_id VARCHAR(256) DEFAULT '' NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (primary_artist_id) REFERENCES aapaca.artists(id)
+);
+
+CREATE TABLE aapaca.external_services (
+    # service_id: amazon -> 1, apple -> 2, spotify -> 3
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE aapaca.external_ids (
+    record_id INT NOT NULL,
+    record_type INT NOT NULL, # record_type: artist -> 1, album -> 2, song -> 3
+    external_id VARCHAR(256) NOT NULL,
+    service_id INT NOT NULL,
+    FOREIGN KEY (service_id) REFERENCES aapaca.external_services(id)
 );
--- mysql/initdb.d/3_init_data.sql
+++ mysql/initdb.d/3_init_data.sql
@@ -1,15 +1,43 @@
-INSERT INTO aapaca.artists (name, birthday, status, description, amazon_music_id, apple_music_id, spotify_id) VALUES ('Common', '1972-03-13', 0, 'Lonnie Corant Jaman Shuka Rashid Lynn Jr. (born March 13, 1972), better known by his stage name Common (formerly Common Sense), is an American rapper, actor and writer.', 'Common/e/B000AQ3K7O', '64490', '2GHclqNVjqGuiE5mA7BEoc');
-INSERT INTO aapaca.artists (name, birthday, status, description, amazon_music_id, apple_music_id, spotify_id) VALUES ('Robert Glasper', '1978-04-05', 0, 'Robert Andre Glasper (born April 6, 1978) is an American pianist and record producer. He has been nominated for seven Grammy Awards and has won three Grammys and one Emmy Award.', 'Robert-Glasper/e/B00197K67U', '4643652', '5cM1PvItlR21WUyBnsdMcn');
-INSERT INTO aapaca.artists (name, birthday, status, description, amazon_music_id, apple_music_id, spotify_id) VALUES ('Karriem Riggins', '1975-08-25', 0, 'Karriem Riggins is an American jazz drummer, hip hop producer, DJ and rapper.', 'Karriem-Riggins/e/B0097A4F46', '5421197', '6e7BQ0gM6o8ecMXRZkXxlZ');
-INSERT INTO aapaca.artists (name, birthday, status, description, amazon_music_id, apple_music_id, spotify_id) VALUES ('August Greene', '2018-01-01', 1, 'August Greene is an American supergroup. The brainchild of rapper Common, and producers Robert Glasper and Karriem Riggins, it was formed in 2018 after sharing the Primetime Emmy Award for Outstanding Original Music and Lyrics for their song...', 'August-Greene/e/B07BS2T1SC', '1372391168', '7KIFh8WDiqlz1Dlopd03cy');
+INSERT INTO aapaca.external_services (name) VALUES ('amazon_music');
+INSERT INTO aapaca.external_services (name) VALUES ('apple_music');
+INSERT INTO aapaca.external_services (name) VALUES ('spotify');
 
-INSERT INTO aapaca.songs (name, primary_artist_id, genre, song_len, amazon_music_id, apple_music_id, spotify_id) VALUES ('Joy and Peace', 1, 'Hip hop', '00:02:40', 'B01MF58RNV', '1440895412?i=1440895418', '5VRq8bpbbSsROTLMTraLNN');
+INSERT INTO aapaca.artists (name, birthday, status, description) VALUES ('Common', '1972-03-13', 0, 'Lonnie Corant Jaman Shuka Rashid Lynn Jr. (born March 13, 1972), better known by his stage name Common (formerly Common Sense), is an American rapper, actor and writer.');
+INSERT INTO aapaca.artists (name, birthday, status, description) VALUES ('Robert Glasper', '1978-04-05', 0, 'Robert Andre Glasper (born April 6, 1978) is an American pianist and record producer. He has been nominated for seven Grammy Awards and has won three Grammys and one Emmy Award.');
+INSERT INTO aapaca.artists (name, birthday, status, description) VALUES ('Karriem Riggins', '1975-08-25', 0, 'Karriem Riggins is an American jazz drummer, hip hop producer, DJ and rapper.');
+INSERT INTO aapaca.artists (name, birthday, status, description) VALUES ('August Greene', '2018-01-01', 1, 'August Greene is an American supergroup. The brainchild of rapper Common, and producers Robert Glasper and Karriem Riggins, it was formed in 2018 after sharing the Primetime Emmy Award for Outstanding Original Music and Lyrics for their song...');
+
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (1, 1, 'Common/e/B000AQ3K7O', 1);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (1, 1, '64490', 2);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (1, 1, '2GHclqNVjqGuiE5mA7BEoc', 3);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (2, 1, 'Robert-Glasper/e/B00197K67U', 1);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (2, 1, '4643652', 2);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (2, 1, '5cM1PvItlR21WUyBnsdMcn', 3);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (3, 1, 'Karriem-Riggins/e/B0097A4F46', 1);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (3, 1, '5421197', 2);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (3, 1, '6e7BQ0gM6o8ecMXRZkXxlZ', 3);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (4, 1, 'August-Greene/e/B07BS2T1SC', 1);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (4, 1, '1372391168', 2);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (4, 1, '7KIFh8WDiqlz1Dlopd03cy', 3);
+
+INSERT INTO aapaca.songs (name, primary_artist_id, genre, song_len) VALUES ('Joy and Peace', 1, 'Hip hop', '00:02:40');
 INSERT INTO aapaca.songs (name, primary_artist_id, genre, song_len) VALUES ('Home', 1, 'Hip hop', '00:03:31');
 INSERT INTO aapaca.songs (name, primary_artist_id, genre, song_len) VALUES ('Word From Moe Luv Interlude', 1, 'Hip hop', '00:00:40');
 
-INSERT INTO aapaca.albums (name, primary_artist_id, label, released_date, description, amazon_music_id, apple_music_id, spotify_id) VALUES ('Black America Again', 1, 'ARTium', '2016-11-04', 'Black America Again is the eleventh studio album by American rapper Common. It was released on November 4, 2016, by ARTium Recordings and Def Jam Recordings.', 'B01MCRSTN4', '1440895412', '2DTbGcv40CSMW0P4yAQQBa');
-INSERT INTO aapaca.albums (name, primary_artist_id, label, released_date, description, amazon_music_id, apple_music_id, spotify_id) VALUES ('August Greene', 4, 'August Greene LLC', '2018-05-04', '', 'B07B6HZ13D', '1372391030', '04o9bBHEVUGabrMRo3vzuy');
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (1, 3, 'B01MF58RNV', 1);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (1, 3, '1440895412?i=1440895418', 2);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (1, 3, '5VRq8bpbbSsROTLMTraLNN', 3);
+
+INSERT INTO aapaca.albums (name, primary_artist_id, label, released_date, description) VALUES ('Black America Again', 1, 'ARTium', '2016-11-04', 'Black America Again is the eleventh studio album by American rapper Common. It was released on November 4, 2016, by ARTium Recordings and Def Jam Recordings.');
+INSERT INTO aapaca.albums (name, primary_artist_id, label, released_date, description) VALUES ('August Greene', 4, 'August Greene LLC', '2018-05-04', '');
 INSERT INTO aapaca.albums (name, primary_artist_id, label, released_date) VALUES ('Let Love', 1, 'Loma Vista', '2019-08-30');
+
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (1, 2, 'B01MCRSTN4', 1);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (1, 2, '1440895412', 2);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (1, 2, '2DTbGcv40CSMW0P4yAQQBa', 3);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (2, 2, 'B07B6HZ13D', 1);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (2, 2, '1372391030', 2);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (2, 2, '04o9bBHEVUGabrMRo3vzuy', 3);
 
 INSERT INTO aapaca.occupations (title) VALUES ('Vocal');
 INSERT INTO aapaca.occupations (title) VALUES ('Guitar');
@@ -64,9 +92,12 @@ INSERT INTO aapaca.songs (name, primary_artist_id) VALUES ('Tsumi to Batsu', 5);
 
 INSERT INTO aapaca.albums (name, primary_artist_id, released_date) VALUES ('Shouso Strip', 5, '2000-03-31');
 INSERT INTO aapaca.albums (name, primary_artist_id, released_date) VALUES ('Adult', 6, '2006-01-25');
-INSERT INTO aapaca.albums (name, primary_artist_id, released_date, apple_music_id, spotify_id) VALUES ('Rekitsu', 8, '2011-03-16', '423534193', '7l9bxNPPXK8458ScKSUXv6');
+INSERT INTO aapaca.albums (name, primary_artist_id, released_date) VALUES ('Rekitsu', 8, '2011-03-16');
 INSERT INTO aapaca.albums (name, primary_artist_id, released_date) VALUES ('Honnou', 5, '1999-10-27');
 INSERT INTO aapaca.albums (name, primary_artist_id, released_date) VALUES ('Kouhukuron', 5, '1998-05-27');
+
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (6, 2, '423534193', 2);
+INSERT INTO aapaca.external_ids (record_id, record_type, external_id, service_id) VALUES (6, 2, '7l9bxNPPXK8458ScKSUXv6', 3);
 
 INSERT INTO aapaca.occupations (title) VALUES ('Vocal');
 INSERT INTO aapaca.occupations (title) VALUES ('Guitar');
