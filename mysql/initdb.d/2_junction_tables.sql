ef230c1b8612a413137eb67f2ab30e4e0b90a7ca
marblet <guitarmarble0117@gmail.com>
2021-01-13 14:19:39 +0900

    enable to change database

--- mysql/initdb.d/2_junction_tables.sql
+++ mysql/initdb.d/2_junction_tables.sql
@@ -1,39 +1,39 @@
-CREATE TABLE aapaca.contents (
+CREATE TABLE contents (
     album_id INT NOT NULL,
     song_id INT NOT NULL,
     song_order VARCHAR(256) NOT NULL,
-    FOREIGN KEY (album_id) REFERENCES aapaca.albums(id),
-    FOREIGN KEY (song_id) REFERENCES aapaca.songs(id)
+    FOREIGN KEY (album_id) REFERENCES albums(id),
+    FOREIGN KEY (song_id) REFERENCES songs(id)
 );
 
-CREATE TABLE aapaca.memberships (
+CREATE TABLE memberships (
     member_id INT NOT NULL,
     group_id INT NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES aapaca.artists(id),
-    FOREIGN KEY (group_id) REFERENCES aapaca.artists(id)
+    FOREIGN KEY (member_id) REFERENCES artists(id),
+    FOREIGN KEY (group_id) REFERENCES artists(id)
 );
 
-CREATE TABLE aapaca.aliases (
+CREATE TABLE aliases (
     artist_id INT NOT NULL,
     alias_artist_id INT NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES aapaca.artists(id),
-    FOREIGN KEY (alias_artist_id) REFERENCES aapaca.artists(id)
+    FOREIGN KEY (artist_id) REFERENCES artists(id),
+    FOREIGN KEY (alias_artist_id) REFERENCES artists(id)
 );
 
-CREATE TABLE aapaca.performances (
+CREATE TABLE performances (
     artist_id INT NOT NULL,
     song_id INT NOT NULL,
     occupation_id INT NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES aapaca.artists(id),
-    FOREIGN KEY (song_id) REFERENCES aapaca.songs(id),
-    FOREIGN KEY (occupation_id) REFERENCES aapaca.occupations(id)
+    FOREIGN KEY (artist_id) REFERENCES artists(id),
+    FOREIGN KEY (song_id) REFERENCES songs(id),
+    FOREIGN KEY (occupation_id) REFERENCES occupations(id)
 );
 
-CREATE TABLE aapaca.participations (
+CREATE TABLE participations (
     artist_id INT NOT NULL,
     album_id INT NOT NULL,
     occupation_id INT NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES aapaca.artists(id),
-    FOREIGN KEY (album_id) REFERENCES aapaca.albums(id),
-    FOREIGN KEY (occupation_id) REFERENCES aapaca.occupations(id)
+    FOREIGN KEY (artist_id) REFERENCES artists(id),
+    FOREIGN KEY (album_id) REFERENCES albums(id),
+    FOREIGN KEY (occupation_id) REFERENCES occupations(id)
 );
