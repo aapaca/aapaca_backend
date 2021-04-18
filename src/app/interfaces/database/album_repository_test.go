8d3d42349bd7b71b36b22f717aa736e046996dab
Hibiki Taguchi <guitarmarble0117@gmail.com>
2021-04-18 16:42:23 +0900

    Update album_repository_test.go

--- src/app/interfaces/database/album_repository_test.go
+++ src/app/interfaces/database/album_repository_test.go
@@ -64,23 +64,6 @@ func (suite *GetAlbumTestSuite) TearDownTest() {
 	}
 }
 
-func assertCredits(t *testing.T, expected []domain.Credit, got []domain.Credit) {
-	assert.Equal(t, len(expected), len(got))
-	// sort expected and got by ArtistID
-	sort.Slice(expected, func(i, j int) bool {
-		p, q := expected[i], expected[j]
-		return p.Artist.ID < q.Artist.ID
-	})
-	sort.Slice(got, func(i, j int) bool {
-		p, q := got[i], got[j]
-		return p.Artist.ID < q.Artist.ID
-	})
-	for i := range expected {
-		assert.Equal(t, expected[i].Artist, got[i].Artist)
-		assert.ElementsMatch(t, expected[i].Parts, got[i].Parts)
-	}
-}
-
 func (suite *GetAlbumTestSuite) TestGetAlbum() {
 	testURL := "http://www.example.com"
 	testDate, _ := time.Parse("2006-01-02", "2021-01-13")
